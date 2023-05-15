9a570d5cb899e8b959e255467b797156e83819ad
Anshika Gupta <g.anshikacreative@gmail.com>
2023-05-16 00:45:13 +0530

    Operators

--- operators.js
+++ operators.js
@@ -1,25 +1,54 @@
-// Expressions And Operators!!
-// Expressions-The combination of operators and operands.
+// // Expressions And Operators!!
+// // Expressions-The combination of operators and operands.
 
-// assignment opertors!!
-var x = 6;
-var y = 7;
-console.log( x == y);
-console.log(`is both the x and y are equal : ${x == y}`);
+// // assignment opertors!!
+// var x = 6;
+// var y = 7;
+// console.log( x == y);
+// console.log(`is both the x and y are equal : ${x == y}`);
   
-// Arithmatic operators!!
-console.log(3+2);
-console.log(10-4);
-console.log(30/2);
-console.log(3*8);
-console.log(19%3);
-console.log("Remainder operator " + 19%8);
+// // Arithmatic operators!!
+// console.log(3+2);
+// console.log(10-4);
+// console.log(30/2);
+// console.log(3*8);
+// console.log(19%3);
+// console.log("Remainder operator " + 19%8);
 
-// Increment and Decrement operator
-var n = 15;
-var p = ++n + 6;
-console.log(n);
-console.log(p);
+// // Increment and Decrement operator
+// var n = 15;
+// var p = ++n + 6;
+// console.log(n);
+// console.log(p);
+
+//Equal operator
+ var a=3;
+ var b=3;
+ console.log(a == b);
+//not equal operator
+var a= 5;
+var b= 4;
+console.log(a == b);
+//Greater than (>)
+var a= 5;
+var b= 4;
+console.log(a > b);
+//less than (<)
+var a= 5;
+var b= 7;
+console.log(a<b);
+//Greater than or equal to(<=)
+var a= 6;
+var b= 6;
+console.log(a>=b);
+//Logical operators
+//logical operators are typically used with boolean (logical) values;
+//when they are,they return a boolean value.
+var a = 30;
+var b = -20;
+console.log( a>b&&a>0);//logical AND (&&) if both conditions are true then true
+console.log(a<b||b<0);//logical OR (||)if only one or more condition true then true
+console.log(!(a>0)&&(b<0));
 
 
 
