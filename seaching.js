25de65b237ce1c2653717aa8a07c888847c82f4c
Dev-anshika98 <g.anshikacreative@gmail.com>
2023-06-22 08:52:02 +0530

    searching

---
+++ seaching.js
@@ -0,0 +1,6 @@
+// Searching and filter in array
+//Array.prototype.indexOf()
+//returns the first (least) index of an element within the array equal to an element, or -1 if none is found.
+//it search the element from the 0th index no.
+
+var myfriends = ['ram','shyam','reeta','run'];
