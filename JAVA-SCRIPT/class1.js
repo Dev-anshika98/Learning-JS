502cb012175e1d238f6d60b38a0c13812e683756
Dev-anshika98 <g.anshikacreative@gmail.com>
2023-09-28 02:04:16 +0530

    .

---
+++ JAVA-SCRIPT/class1.js
@@ -0,0 +1,55 @@
+//Data type
+//string - with single and double codes
+
+var x = 'anshika';
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
+
+//number
+document.write("<br>");
+var x = 34;
+document.write(x);
+document.write("<br>");
+document.write(typeof x)
+
+
+//Boolean
+document.write("<br>");
+
+var x = true; //false
+document.write(x);
+document.write("<br>");
+document.write(typeof x)
+
+//Undefined
+document.write("<br>");
+var y;
+document.write(y);
+document.write("<br>");
+document.write(typeof y);
+
+//Array
+document.write("<br>");
+var z=["HTML", "CSS"];
+document.write(z);
+document.write("<br>");
+document.write(typeof z);
+
+//Object
+a = {
+    name: "Riya",
+    country:"India",
+
+}
+document.write("<br>");
+document.write(a);
+document.write("<br>");
+document.write(typeof a);
+
+//null
+document.write("<br>");
+x= null;
+document.write(x)
+document.write(typeof x);
+
