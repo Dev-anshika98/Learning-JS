04cd18610d4dd0e2dde73536a506b57033324fb3
Dev-anshika98 <g.anshikacreative@gmail.com>
2023-10-01 17:39:02 +0530

    some more properties

---
+++ JAVA-SCRIPT/class2.js
@@ -0,0 +1,73 @@
+// var x =50;
+// console.error("something wrong.");
+// console.time("test");
+// console.warn("this is warning");
+// console.warn("this is warning");
+// console.warn("this is warning");
+// console.warn("this is warning");
+// console.warn("this is warning");
+// console.timeEnd("test");
+
+//operators
+// var a = 20;
+// var b = "20";
+// if(a===b){
+//     document.write("A is greater");
+// }
+// //logical operators
+// var age = 22;
+// if(!age<25){
+//     console.log("u r eligible");
+// }
+
+
+
+// //Alert box
+// var a= 50;
+// var b= 20;
+// if(a>b){
+//     alert("you are dumb :"+ a);
+// }
+// else{
+//     alert("you are champak" + b);
+// }
+
+
+////confirm box
+// var a = confirm("do u like me??")
+
+// if(a){
+//     alert("thanks");
+// }
+// else{
+//     alert("sorry");
+// }
+
+
+//Prompt Box
+// var a = prompt("what is your name?");
+// alert(a)
+// document.write(a);
+
+
+// var per = prompt("enter your marks");
+// if(per >=80 || per<=70){
+//     document.write("you are in 1st div");
+// }
+// else if(per<=50){
+//     document.write("you are fail");
+// }
+
+
+//Functions
+function hello(){
+document.write("tum pagal ho");
+}
+function hola(){
+    document.write("Adesh Panday");
+    }
+hello();
+document.write("<br>");
+hello();
+hola();
+hola();
